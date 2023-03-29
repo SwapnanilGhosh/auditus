5f529b6fabb40f836c3f75593326060ef5456940
SwapnanilGhosh <swapnanil2002@gmail.com>
2023-03-30 00:41:10 +0530

    Update

--- form.js
+++ form.js
@@ -46,14 +46,8 @@ function sendMessage() {
     submitBtn.innerHTML = "Audit";
     submitBtn.disabled = false;
     chatBoxBody.classList.add("information");
-    console.log(data);
-    var a = data.message.split(/([0-9])\./g);
-    a.pop();
-    var toAppend = "";
-    a.forEach(function (t, i){
-        toAppend += t + "<br>";
-    })
-    chatBoxBody.innerHTML = `<h3>Audited Report</h3><br><p>${toAppend}</p>`;
+    
+    chatBoxBody.innerHTML = `<h3>Audited Report</h3><br><p>${data.message}</p>`;
     scrollToBottom();
   });
 }
